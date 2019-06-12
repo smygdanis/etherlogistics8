9f8a5b422425db163368c1ede3d892087f580a1d
unknown <mygdanis@etherlogic.gr>
2019-06-12 19:45:45 +0300

    firebasekeys added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: true,
-    hmr       : false
+    hmr: false,
+    firebase: {
+        apiKey: "AIzaSyBjgukthOrB73u8dpf_dzFoAA4qhTidPvQ",
+        authDomain: "etherlogistics-3675d.firebaseapp.com",
+        databaseURL: "https://etherlogistics-3675d.firebaseio.com",
+        projectId: "etherlogistics-3675d",
+        storageBucket: "etherlogistics-3675d.appspot.com",
+        messagingSenderId: "905915998291"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
     production: false,
-    hmr       : false
+    hmr: false,
+    firebase: {
+        apiKey: "AIzaSyBjgukthOrB73u8dpf_dzFoAA4qhTidPvQ",
+        authDomain: "etherlogistics-3675d.firebaseapp.com",
+        databaseURL: "https://etherlogistics-3675d.firebaseio.com",
+        projectId: "etherlogistics-3675d",
+        storageBucket: "etherlogistics-3675d.appspot.com",
+        messagingSenderId: "905915998291"
+    }
 };
 
 /*
