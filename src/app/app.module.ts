bdf529757b6bebb47547e83d5248c3bc54c3d31b
unknown <mygdanis@etherlogic.gr>
2019-06-12 19:55:56 +0300

    auth and dialog service added, angularfire2 installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,12 @@ import { AppComponent } from "app/app.component";
 import { LayoutModule } from "app/layout/layout.module";
 import { SampleModule } from "app/main/sample/sample.module";
 
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
 const appRoutes: Routes = [
     {
         path: "**",
@@ -34,6 +40,8 @@ const appRoutes: Routes = [
 import { AsanaService } from "app/services/asana.service";
 import { ToastrService } from "app/services/toastr.service";
 import { FirebaseService } from "app/services/firebase.service";
+import { DialogService } from "app/services/dialog.service";
+import { AuthService } from "app/services/auth.service";
 
 //TODO: AUTH SERVICE
 //TODO: DIALOG MODAL SERVICE
@@ -43,6 +51,10 @@ import { FirebaseService } from "app/services/firebase.service";
     declarations: [AppComponent],
     imports: [
         BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase, "EtherLogistics"), // imports firebase/app needed for everything
+        AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+        AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+        AngularFireStorageModule, // imports firebase/storage only needed for storage features
         BrowserAnimationsModule,
         HttpClientModule,
         RouterModule.forRoot(appRoutes),
@@ -67,7 +79,13 @@ import { FirebaseService } from "app/services/firebase.service";
         LayoutModule,
         SampleModule
     ],
-    providers: [AsanaService, ToastrService, FirebaseService],
+    providers: [
+        AsanaService,
+        ToastrService,
+        FirebaseService,
+        DialogService,
+        AuthService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
