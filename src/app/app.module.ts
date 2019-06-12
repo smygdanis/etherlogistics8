2e2c43b310e4f98cfb9139f628f3ca121d01fb08
unknown <mygdanis@etherlogic.gr>
2019-06-12 19:50:14 +0300

    services added @ app.module.ts, firebase lib added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { TranslateModule } from '@ngx-translate/core';
-import 'hammerjs';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule, Routes } from "@angular/router";
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { TranslateModule } from "@ngx-translate/core";
+import "hammerjs";
 
-import { FuseModule } from '@fuse/fuse.module';
-import { FuseSharedModule } from '@fuse/shared.module';
-import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
+import { FuseModule } from "@fuse/fuse.module";
+import { FuseSharedModule } from "@fuse/shared.module";
+import {
+    FuseProgressBarModule,
+    FuseSidebarModule,
+    FuseThemeOptionsModule
+} from "@fuse/components";
 
-import { fuseConfig } from 'app/fuse-config';
+import { fuseConfig } from "app/fuse-config";
 
-import { AppComponent } from 'app/app.component';
-import { LayoutModule } from 'app/layout/layout.module';
-import { SampleModule } from 'app/main/sample/sample.module';
+import { AppComponent } from "app/app.component";
+import { LayoutModule } from "app/layout/layout.module";
+import { SampleModule } from "app/main/sample/sample.module";
 
 const appRoutes: Routes = [
     {
-        path      : '**',
-        redirectTo: 'sample'
+        path: "**",
+        redirectTo: "sample"
     }
 ];
 
+// SERVICES
+import { AsanaService } from "app/services/asana.service";
+import { ToastrService } from "app/services/toastr.service";
+import { FirebaseService } from "app/services/firebase.service";
+
+//TODO: AUTH SERVICE
+//TODO: DIALOG MODAL SERVICE
+//TODO: ROUTING MODULE
+
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports     : [
+    declarations: [AppComponent],
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
@@ -56,10 +67,7 @@ const appRoutes: Routes = [
         LayoutModule,
         SampleModule
     ],
-    bootstrap   : [
-        AppComponent
-    ]
+    providers: [AsanaService, ToastrService, FirebaseService],
+    bootstrap: [AppComponent]
 })
-export class AppModule
-{
-}
+export class AppModule {}
